1de74bbd40ee84c5a9ddd39a9db4e17d6fbd1557
mx5566 <2473321894@qq.com>
2021-04-14 16:15:09 +0800

    1

---
+++ Assets/Space Shooter Template FREE/Scripts/Bonus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class Bonus : MonoBehaviour {
+
+    public Action<Collider2D, Bonus> OnTriggerEnter2DLua;
+
+    //when colliding with another object, if another object is 'Player', sending command to the 'Player'
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (OnTriggerEnter2DLua != null)
+        {
+            OnTriggerEnter2DLua(collision, this);
+        }
+        if (collision.CompareTag("Player")) 
+        {
+            if (PlayerShooting.instance.weaponPower < PlayerShooting.instance.maxweaponPower)
+            {
+                PlayerShooting.instance.weaponPower++;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
